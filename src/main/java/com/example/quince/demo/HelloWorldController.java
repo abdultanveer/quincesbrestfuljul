746a9aa4eddef776525e10748bdf05586e9f114b
abdul <abdul.tanveer@gmail.com>
2026-06-26 11:17:18 +0530

    initial commit

---
+++ src/main/java/com/example/quince/demo/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.quince.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "quince Hello World!";
+    }
+}
